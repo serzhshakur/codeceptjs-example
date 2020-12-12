7cb0806ed304914399752a70f8a63a31fbb9714b
Sergey Shakur <s.sakurs@bilderlings.com>
2020-12-12 11:54:23 +0200

    bumping up pollyjs version to make Polly properly interact with Puppeteer

--- tests/search_test.js
+++ tests/search_test.js
@@ -1,9 +1,12 @@
-Feature('Search');
+Feature('Search in Duckduckgo');
 
 const response = [{"phrase": "test"}, {"phrase": "hello"}, {"phrase": "world"}, {"phrase": "duckduckgo"}];
 
-Scenario('search in Duckduckgo', ({I}) => {
+Scenario('search a word with mocked response', ({I}) => {
     I.amOnPage('https://duckduckgo.com/');
-    I.mockRequest('GET', 'https://duckduckgo.com/ac/*', response);
+    I.mockRequest('GET', 'https://duckduckgo.com/ac/?q*', response);
     I.fillField('#search_form_input_homepage', 'hello');
+    I.seeElement('.search__autocomplete');
+    I.seeNumberOfElements('.search__autocomplete .acp', response.length);
+    response.forEach(({phrase}) => I.see(phrase, '.search__autocomplete .acp'))
 });
